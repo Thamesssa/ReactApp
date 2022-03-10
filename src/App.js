abd5c4c878dd0cf37481357772d168ffe1ebbe82
thamesssa <thamesssa@gmail.com>
2022-03-10 14:05:45 +0200

    Update App.js
    adde a list of dishes using jsx props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import { Component } from 'react';
+import React,{ Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
+
+
 
 class  App extends Component {
   render() { 
     return(
+      
     <div className="App">
       <Navbar dark color = "primary">
         <div>
           <NavbarBrand href="/">Resturant Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
+    
   );
     }
 }
