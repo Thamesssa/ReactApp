8c847695f092017c3fd0799d3274166cd5322b8b
thamesssa <thamesssa@gmail.com>
2022-03-10 10:29:24 +0200

    intitial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
-import Menu from './components/MenuComponent'
+import { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
 
-<Menu/>
+class  App extends Component {
+  render() { 
+    return(
+    <div className="App">
+      <Navbar dark color = "primary">
+        <div>
+          <NavbarBrand href="/">Resturant Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+    </div>
+  );
+    }
+}
+
+export default App;
